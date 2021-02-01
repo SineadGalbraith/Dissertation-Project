3a2a6f84a84d2b97ffcb2e3b09dc858f381b3074
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2021-02-01 18:54:23 +0000

    Basic welcome screen added. Empty activity for variable introduction added.

---
+++ app/src/main/java/com/dissertation/WelcomeScreen.kt
@@ -0,0 +1,25 @@
+package com.dissertation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class WelcomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+
+        val welcomeButton = findViewById<Button>(R.id.PlayButton)
+            welcomeButton.setOnClickListener{
+                val intent = Intent(this, VariableStartingPage::class.java)
+                startActivity(intent)
+            }
+    }
+
+
+
+
+
+}
