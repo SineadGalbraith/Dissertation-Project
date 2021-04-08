ebd89514b8a12f6ba959a5ffa79d628362dc7cb8
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2021-04-08 13:01:48 +0100

    Updated Create Character Screen Layout File

--- app/src/main/java/com/dissertation/CreateCharacterScreen.kt
+++ app/src/main/java/com/dissertation/CreateCharacterScreen.kt
@@ -19,41 +19,44 @@ import javax.microedition.khronos.opengles.GL10
 class CreateCharacterScreen : AppCompatActivity() , DialogInterface.OnDismissListener {
     private val introductionDialog = CreateCharacterIntroductionParentFragment()
 
-    // Surface View
-    private lateinit var characterSurfaceView : GLSurfaceView
-    private var finishedButton : ImageButton? = null
-    // List of Models
-    private var ball : Ball? = null
+//    // Surface View
+//    private lateinit var characterSurfaceView : GLSurfaceView
+//    private var finishedButton : ImageButton? = null
+//    // List of Models
+//    private var ball : Ball? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_character_screen)
+
+        // Name Input Dialog Fragment
         introductionDialog.show(supportFragmentManager, "enter_name_introduction")
-        finishedButton = findViewById(R.id.createCharacterScreenFinishedButton)
-
-        characterSurfaceView = findViewById(R.id.characterSurfaceView)
-        characterSurfaceView.setEGLContextClientVersion(2)
-        characterSurfaceView.setRenderer(object : GLSurfaceView.Renderer {
-            override fun onDrawFrame(gl: GL10?) {
-                ball?.draw()
-            }
-
-            override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-                GLES20.glViewport(0,0, width, height);
-            }
-
-            override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-                characterSurfaceView.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY;
-                ball = Ball(applicationContext);
-            } // More code goes here
-        })
-
-        finishedButton?.setOnClickListener {
-            val intent = Intent(this, GameStyleChoiceScreen::class.java)
-            startActivity(intent)
-        }
 
 
+
+//        finishedButton = findViewById(R.id.createCharacterScreenFinishedButton)
+//
+//        characterSurfaceView = findViewById(R.id.characterSurfaceView)
+//        characterSurfaceView.setEGLContextClientVersion(2)
+//        characterSurfaceView.setRenderer(object : GLSurfaceView.Renderer {
+//            override fun onDrawFrame(gl: GL10?) {
+//                ball?.draw()
+//            }
+//
+//            override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+//                GLES20.glViewport(0,0, width, height);
+//            }
+//
+//            override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+//                characterSurfaceView.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY;
+//                ball = Ball(applicationContext);
+//            } // More code goes here
+//        })
+//
+//        finishedButton?.setOnClickListener {
+//            val intent = Intent(this, GameStyleChoiceScreen::class.java)
+//            startActivity(intent)
+//        }
 //        val ballObj = ObjLoader(this, "src/main/assets/ball.obj")
 //        val ballModel = Model(ballObj)
 //        modelList.add(ballModel)
@@ -89,15 +92,8 @@ class CreateCharacterScreen : AppCompatActivity() , DialogInterface.OnDismissLis
             .transparentOverlay(false)
             .showArrow(false)
             .overlayWindowBackgroundColor(Color.BLACK)
-//            .onDismissListener {
-//                buildCharacter()
-//            }
             .build()
             .show()
     }
-//
-//    private fun buildCharacter() {
-////        choicesDialog.show(supportFragmentManager, "avatar_choices")
-//    }
 
 }
